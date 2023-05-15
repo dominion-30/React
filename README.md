caafed429cf2849ab9bbdac04d167d51eee30440
dominion-30 <abiolaayobamidele@gmail.com>
2023-05-15 16:17:55 +0100

    Add file

---
+++ README.md
@@ -0,0 +1,3 @@
+This repository contains projects and tasks from my React journey Using Fullstack React: The Complete Guide to ReactJS and Friends
+Written by Anthony Accomazzo, Nate Murray, Ari Lerner, Clay
+Allsopp, David Guttman, and Tyler McGinnis
---
+++ voting_app/public/js/app-1.js
@@ -0,0 +1,33 @@
+// class ProductList extends React.Component {
+//   render() {
+//     return (
+//       <div className='ui unstackable items'>
+//         Hello, friend! I am a basic React component.
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <ProductList />,
+//   document.getElementById('content')
+// );
+
+
+
+// class ProductList extends React.Component{
+//   render(){
+//     return (
+//       <div className="ui unstackable items">
+//         Hello, friend! I am a basic React component.
+//       </div>
+//     );
+//   }
+// }
+
+
+class ProductList extends React.Component{
+  render(){
+    return(<div className="ui unstackable items">Hello, friends! I am a basic React component</div>);
+  }
+}
