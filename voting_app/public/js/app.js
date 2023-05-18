294a071cfb89f4b7b8555d2cca03f278ca28e9c6
dominion-30 <abiolaayobamidele@gmail.com>
2023-05-18 12:11:49 +0100

    Add file

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -88,7 +88,7 @@
 // This is the start of the project in Full Stack React
 
 
-
+// React Journey
 
 // class ProductList extends React.Component{
 //   render(){
@@ -100,33 +100,66 @@
 
 
 
+class ProductList extends React.Component{
+  render(){
+    const product = Seed.products[0]
+    return(<div className="ui unstackable items">
+      <Product 
+      id={product.id}
+      title={product.title}
+      description={product.description}
+      url={product.url}
+      votes={product.votes}
+      submitterAvatarUrl={product.submitterAvatarUrl}
+      productImageUrl={product.productImageUrl}/>
+    </div>
 
-// class Product extends React.Component{
-//   render(){
-//     return(
-//     <div className="item">
-//       <div className="image">
-//         <img src="images/products/image-aqua.png"/>
-//       </div>
-
-//       <div className="middle aligned content">
-//         <div className='description'>
-//           <a>Fort Knight</a>
-//           <p>Authentic renaissance actors, delivered in just two week\s</p>
-//         </div>
-
-//         <div className="extra">
-//         <span>Submitted by:</span>
-//         <img className="ui avatar image" src="images/avatars/daniel.jpg"/>
-//       </div>
+    );
+  }
+}
 
 
-//       </div>
+
+class Product extends React.Component{
+  render(){
+    return(
+    <div className="item">
+      <div className="image">
+        <img src={this.props.productImageUrl}/>
+      </div>
+
+      <div className="middle aligned content">
+        <div className='header'>
+          <a>
+            <i className="large caret up icon"/>
+          </a>
+          {this.props.votes}
+          
+        </div>
+
+        <div className="description">
+          <a href={this.props.url}>
+            {this.props.title}  
+          </a> 
+
+          <p>
+            {this.props.description}
+          </p>
+
+        </div>
+
+        <div className="extra">
+        <span>Submitted by:</span>
+        <img className="ui avatar image" src={this.props.submitterAvatarUrl}/>
+      </div>
+
+
+      </div>
 
     
-//     </div>);
-//   }
-// }
+    </div>);
+  }
+}
 
 
-// ReactDOM.render(<ProductList/>, document.getElementById('content'));
+ReactDOM.render(<ProductList/>, document.getElementById('content'));
