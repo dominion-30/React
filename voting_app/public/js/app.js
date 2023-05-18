1c6f72236f56ae9193e48843cefc336601ee2595
dominion-30 <abiolaayobamidele@gmail.com>
2023-05-18 14:22:01 +0100

    Add file

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -98,11 +98,76 @@
 //   }
 // }
 
+// Data Driven Model
+
+// class ProductList extends React.Component{
+//   render(){
+//     const product = Seed.products[3]
+//     return(<div className="ui unstackable items">
+//       <Product 
+//       id={product.id}
+//       title={product.title}
+//       description={product.description}
+//       url={product.url}
+//       votes={product.votes}
+//       submitterAvatarUrl={product.submitterAvatarUrl}
+//       productImageUrl={product.productImageUrl}/>
+//     </div>
+
+//     );
+//   }
+// }
+
+
+
+// class Product extends React.Component{
+//   render(){
+//     return(
+//     <div className="item">
+//       <div className="image">
+//         <img src={this.props.productImageUrl}/>
+//       </div>
+
+//       <div className="middle aligned content">
+//         <div className='header'>
+//           <a>
+//             <i className="large caret up icon"/>
+//           </a>
+//           {this.props.votes}
+          
+//         </div>
+
+//         <div className="description">
+//           <a href={this.props.url}>
+//             {this.props.title}  
+//           </a> 
+
+//           <p>
+//             {this.props.description}
+//           </p>
+
+//         </div>
+
+//         <div className="extra">
+//         <span>Submitted by:</span>
+//         <img className="ui avatar image" src={this.props.submitterAvatarUrl}/>
+//       </div>
+
+
+//       </div>
+
+    
+//     </div>);
+//   }
+// }
+
+
+// ReactDOM.render(<ProductList/>, document.getElementById('content'));
 
 
 class ProductList extends React.Component{
   render(){
-    const product = Seed.products[3]
+    const product = Seed.products[0];
     return(<div className="ui unstackable items">
       <Product 
       id={product.id}
@@ -112,54 +177,43 @@ class ProductList extends React.Component{
       votes={product.votes}
       submitterAvatarUrl={product.submitterAvatarUrl}
       productImageUrl={product.productImageUrl}/>
-    </div>
-
-    );
-  }
-}
-
-
-
-class Product extends React.Component{
-  render(){
-    return(
-    <div className="item">
-      <div className="image">
-        <img src={this.props.productImageUrl}/>
-      </div>
-
-      <div className="middle aligned content">
-        <div className='header'>
-          <a>
-            <i className="large caret up icon"/>
-          </a>
-          {this.props.votes}
-          
-        </div>
-
-        <div className="description">
-          <a href={this.props.url}>
-            {this.props.title}  
-          </a> 
-
-          <p>
-            {this.props.description}
-          </p>
-
-        </div>
-
-        <div className="extra">
-        <span>Submitted by:</span>
-        <img className="ui avatar image" src={this.props.submitterAvatarUrl}/>
-      </div>
-
-
-      </div>
-
-    
     </div>);
   }
 }
 
+class Product extends React.Component{
+  render(){
+    return(
+      <div className="item">
+        <div className="image">
+          <img src={this.props.productImageUrl}/>
+        </div>
+        <div className="middle aligned content">
+          <div className="header">
+            <a>
+              <i className="large caret up icon"/>
+            </a>
+            {this.props.votes}
+          </div>
 
-ReactDOM.render(<ProductList/>, document.getElementById('content'));
+          <div className="description">
+          <a href={this.props.url}>
+            {this.props.title}
+          </a>
+          <p>
+            {this.props.description}
+          </p>
+        </div>
+
+        <div className="extra">
+          <span>Submitted by:</span>
+          <img className="ui avatar image" src={this.props.submitterAvatarUrl}/>
+        </div>
+        </div>
+       
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ProductList/>, document.getElementById("content"));
