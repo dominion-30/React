52a241832f6e04b1c80595ffb3fbac6e149b02ab
dominion-30 <abiolaayobamidele@gmail.com>
2023-05-23 12:40:35 +0100

    Add changes

--- voting_app/public/js/app.js
+++ voting_app/public/js/app.js
@@ -245,7 +245,12 @@
 // ReactDOM.render(<ProductList/>, document.getElementById("content"));
 
 class ProductList extends React.Component{
+  handleProductUpVote(productId){
+    console.log(productId + "was upvoted.");
+  }
   render(){
+   
+    const products = Seed.products.sort((a,b)=>(b.votes - a.votes));
     const productComponents = Seed.products.map((product)=>(<Product
       key={'product-' + product.id}
       id={product.id}
@@ -255,6 +260,7 @@ class ProductList extends React.Component{
       votes={product.votes}
       submitterAvatarUrl={product.submitterAvatarUrl}
       productImageUrl={product.productImageUrl}
+      onVote = {this.handleProductUpVote}
      /> 
       
       ));
@@ -267,6 +273,9 @@ class ProductList extends React.Component{
 }
 
 class Product extends React.Component{
+  handleUpVote(){
+    this.props.onVote(this.props.id);
+  }
   render(){
     return(
       <div className="item">
@@ -276,7 +285,7 @@ class Product extends React.Component{
 
         <div className="middle aligned content">
           <div className="header">
-            <a>
+            <a onClick={this.handleUpVote}>
               <i className="large caret up icon"/>
             </a>
             {this.props.votes}
